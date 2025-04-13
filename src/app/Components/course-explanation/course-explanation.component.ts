cec338a3a4039f9f276698865bb5747a485095a7
norhan <norhanmahmoud447@gmail.com>
2025-04-13 22:18:05 +0200

    modify the json server and the interface of Icareercourses and the courseExplanation component

--- src/app/Components/course-explanation/course-explanation.component.ts
+++ src/app/Components/course-explanation/course-explanation.component.ts
@@ -1,31 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
-import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { ServiceWithApiService } from '../../Services/service-with-api.service';
 import { ICareerCourses } from '../../Models/ICareerCourses';
 import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-course-explanation',
-  imports: [CommonModule],
   templateUrl: './course-explanation.component.html',
-  styleUrl: './course-explanation.component.scss'
+  imports: [
+    CommonModule,
+  ],
+  styleUrls: ['./course-explanation.component.scss']
 })
 export class CourseExplanationComponent implements OnInit {
-  selectedSection = 'module1'; 
-  isMaterialCollapsed = true; 
+  selectedSection: string | null = null;
+  isMaterialCollapsed = true;
   CourseId: string = '';
-  CareerCourse:ICareerCourses| undefined = undefined;
- constructor( 
+  CareerCourse: ICareerCourses | undefined = undefined;
+  isCollapsed = true;
+
+
+  constructor(
     private CourseService: ServiceWithApiService,
-       private activatedroute: ActivatedRoute) {}
+    private activatedroute: ActivatedRoute,
+    private sanitizer: DomSanitizer
+  ) {}
+
   ngOnInit(): void {
     this.CourseId = this.activatedroute.snapshot.paramMap.get('CourseId')
-    ? String(this.activatedroute.snapshot.paramMap.get('CourseId'))
-    : '';
-     console.log(this.CourseId);
-    
-   this.GetCareerCourseById() 
+      ? String(this.activatedroute.snapshot.paramMap.get('CourseId'))
+      : '';
+    this.GetCareerCourseById();
   }
 
   toggleCourseMaterial() {
@@ -36,15 +42,21 @@ export class CourseExplanationComponent implements OnInit {
     this.CourseService.getCarerrCourseById(this.CourseId).subscribe({
       next: (data) => {
         this.CareerCourse = data;
-        console.log( this.CareerCourse);
 
-      
+        // to make the first module to be the default 
+        if (this.CareerCourse?.modules?.length > 0) {
+          this.selectedSection = this.CareerCourse.modules[0].moduleTitle; //to display the title of the module as default 
+
+        }
       },
       error: (err) => {
         console.error('Error fetching career course:', err);
       },
     });
   }
-  
 
+  // Method to sanitize video URLs
+  sanitizeVideoUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
--- src/app/Models/ICareerCourses.ts
+++ src/app/Models/ICareerCourses.ts
@@ -1,19 +1,23 @@
 export interface ICareerCourses {
-    id: string;
+  id: string;
+  name: string;
+  description: string;
+  categoryID: number;
+  imgUrl: string;
+  IfYouLike: string;
+  IfYouLikeValue: string;
+  SkillsNeeded: string;
+  SkillsNeededValue: string;
+  logoImage: string;
+  organization: string;
+  relatedCourses: {
+    RelatedCourseID: string;
     name: string;
-    description: string;
-    categoryID: number;
     imgUrl: string;
-    IfYouLike: string;
-    IfYouLikeValue: string;
-    SkillsNeeded: string;
-    SkillsNeededValue: string;
-    logoImage: string;
-      
-    organization:string,
-    relatedCourses: {
-      RelatedCourseID: string;
-      name: string;
-      imgUrl: string;
-    }[];  
-  }
+  }[];
+  modules: {
+    id: string; 
+    moduleTitle: string;
+    videos: string[];
+  }[];
+}
