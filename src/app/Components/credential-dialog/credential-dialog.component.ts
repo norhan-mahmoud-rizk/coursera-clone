caf5d67378da661e3320bcb7c46f107f465b3125
norhan <norhanmahmoud447@gmail.com>
2025-04-26 22:17:26 +0300

    add new dialogs of the edit and project and credential and change in the profile component by norhan

---
+++ src/app/Components/credential-dialog/credential-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-credential-dialog',
+  imports: [],
+  templateUrl: './credential-dialog.component.html',
+  styleUrl: './credential-dialog.component.scss'
+})
+export class CredentialDialogComponent {
+
+}
---
+++ src/app/Components/project-dialog/project-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-dialog',
+  imports: [],
+  templateUrl: './project-dialog.component.html',
+  styleUrl: './project-dialog.component.scss'
+})
+export class ProjectDialogComponent {
+
+}
