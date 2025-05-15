e4762f96d567847ed0fddb0ba4b09645bab92333
norhan <norhanmahmoud447@gmail.com>
2025-05-15 12:07:46 +0300

    change the images to new interface in all components by norhan

--- src/app/Models/ICareerCourses.ts
+++ src/app/Models/ICareerCourses.ts
@@ -3,7 +3,7 @@ export interface ICareerCourses {
   name: string;
   description: string;
   categoryID: number;
-  imgUrl: string;
+  courseImage: string;
   IfYouLike: string;
   IfYouLikeValue: string;
   SkillsNeeded: string;
