00eaac986650c8272ac17dbb08dce9017e3854a3
MarinaMousa <marinamousa73@gmail.com>
2025-05-17 19:00:26 +0300

    Refactor video player routing and component to use dynamic parameters; update assignment model structure for improved data handling

--- src/app/Components/video-player/video-player.component.ts
+++ src/app/Components/video-player/video-player.component.ts
@@ -3,7 +3,8 @@ import { CommonModule, Location } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CourseData, Module, Topic, Video } from '../../Models/course-details';
 import { CoursesService } from '../../Services/courses.service';
-import { tap } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-video-player',
@@ -12,6 +13,11 @@ import { tap } from 'rxjs';
   imports: [CommonModule, NavbarComponent],
 })
 export class VideoPlayerComponent implements OnInit {
+  courseId: string = '';
+  moduleId: string = '';
+  topicId: string = '';
+  videoId: string = '';
+
   course?: CourseData;
   module?: Module;
   topic?: Topic;
@@ -22,45 +28,82 @@ export class VideoPlayerComponent implements OnInit {
 
   constructor(
     private location: Location,
-    private coursesService: CoursesService
+    private coursesService: CoursesService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    const navigation = this.location.getState() as any;
+    this.route.params
+      .pipe(
+        map((params) => {
+          this.courseId = params['courseId'];
+          this.moduleId = params['moduleId'];
+          this.topicId = params['topicId'];
+          this.videoId = params['videoId'];
+          return {
+            courseId: this.courseId,
+            moduleId: this.moduleId,
+            topicId: this.topicId,
+            videoId: this.videoId,
+          };
+        }),
+        switchMap((params) => {
+          return this.coursesService.getCourseDetails(params.courseId);
+        }),
+        tap((courseDetails) => {
+          this.course = courseDetails;
+          this.module = courseDetails.modules.find(
+            (module) => module._id === this.moduleId
+          );
+          this.topic = this.module?.topics.find(
+            (topic) => topic._id === this.topicId
+          );
+          this.video = this.topic?.videos.find(
+            (video) => video._id === this.videoId
+          );
 
-    this.course = navigation?.course;
-    this.module = navigation?.module;
-    this.topic = navigation?.topic;
-    this.video = navigation?.video;
-
-    if (this.course && this.module && this.video && this.topic) {
-      this.currentVideoList = this.module.topics.flatMap((x) => x.videos);
-      this.currentVideoIndex = this.currentVideoList.findIndex(
-        (v) => v._id === this.video?._id
-      );
-    }
+          if (this.course && this.module && this.video && this.topic) {
+            this.currentVideoList = this.module.topics.flatMap((x) => x.videos);
+            this.currentVideoIndex = this.currentVideoList.findIndex(
+              (v) => v._id === this.video?._id
+            );
+            this.videoPlayer.nativeElement.load();
+          }
+        })
+      )
+      .subscribe();
   }
 
   goToNextVideo() {
     if (this.currentVideoIndex < this.currentVideoList.length - 1) {
       this.currentVideoIndex++;
-      this.video = this.currentVideoList[this.currentVideoIndex];
-      this.videoPlayer.nativeElement.load();
+      this.navigateToVideo();
     }
   }
 
   goToPreviousVideo() {
     if (this.currentVideoIndex > 0) {
       this.currentVideoIndex--;
-      this.video = this.currentVideoList[this.currentVideoIndex];
-      this.videoPlayer.nativeElement.load();
+      this.navigateToVideo();
     }
+  }
+
+  private navigateToVideo() {
+    this.video = this.currentVideoList[this.currentVideoIndex];
+    this.videoId = this.currentVideoList[this.currentVideoIndex]._id;
+    this.topic = this.module?.topics?.find((topic) =>
+      topic.videos.find((video) => video._id === this.videoId) ? topic._id : ''
+    );
+    this.topicId = this.topic?._id || '';
+    this.location.replaceState(
+      `/courseExplanation/${this.courseId}/video-player/${this.moduleId}/${this.topicId}/${this.videoId}`
+    );
+    this.videoPlayer.nativeElement.load();
   }
 
   selectVideo(index: number) {
     this.currentVideoIndex = index;
-    this.video = this.currentVideoList[this.currentVideoIndex];
-    this.videoPlayer.nativeElement.load();
+    this.navigateToVideo();
   }
 
   @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
