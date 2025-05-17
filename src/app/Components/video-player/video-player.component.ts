091a4b6f19cbf4aeafbb48a038d3ced3c6c7616f
MarinaMousa <marinamousa73@gmail.com>
2025-05-17 22:17:33 +0300

    Implement assignment handling in video player component; update routing and service methods for assignment retrieval and submission

--- src/app/Components/video-player/video-player.component.ts
+++ src/app/Components/video-player/video-player.component.ts
@@ -7,22 +7,33 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { CourseData, Module, Topic, Video } from '../../Models/course-details';
-import { CoursesService } from '../../Services/courses.service';
-import { map, switchMap, tap } from 'rxjs';
+import {
+  Assignment,
+  CourseData,
+  Module,
+  Topic,
+  Video,
+} from '../../Models/course-details';
+import {
+  AssignmentResponse,
+  CoursesService,
+} from '../../Services/courses.service';
+import { catchError, map, switchMap, tap } from 'rxjs';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.scss'],
-  imports: [CommonModule, NavbarComponent, RouterModule],
+  imports: [CommonModule, NavbarComponent, RouterModule, FormsModule],
 })
 export class VideoPlayerComponent implements OnInit, AfterViewChecked {
   courseId: string = '';
   moduleId: string = '';
   topicId: string = '';
-  videoId: string = '';
+  videoId?: string;
+  assignmentId?: string;
 
   nextVideoRoute?: { route: string; queryParams: any };
   previousVideoRoute?: { route: string; queryParams: any };
@@ -34,6 +45,9 @@ export class VideoPlayerComponent implements OnInit, AfterViewChecked {
   module?: Module;
   topic?: Topic;
   video?: Video;
+  assignement?: AssignmentResponse;
+  userAnswers: any;
+  errorMessage: any;
 
   constructor(
     private coursesService: CoursesService,
@@ -41,7 +55,7 @@ export class VideoPlayerComponent implements OnInit, AfterViewChecked {
     private router: Router
   ) {}
   ngAfterViewChecked(): void {
-    this.videoPlayer.nativeElement.load();
+    this.videoPlayer?.nativeElement.load();
   }
 
   ngOnInit(): void {
@@ -70,12 +84,23 @@ export class VideoPlayerComponent implements OnInit, AfterViewChecked {
           );
           this.contentRoutingList =
             this.module?.topics?.flatMap((topic) => {
-              return topic.videos.map((video) => {
-                return {
-                  route: `/courseExplanation/${this.courseId}/video-player/${this.moduleId}/${topic._id}`,
-                  queryParams: { videoId: video._id },
-                };
-              });
+              return topic.videos
+                .map((video) => {
+                  return {
+                    route: `/courseExplanation/${this.courseId}/video-player/${this.moduleId}/${topic._id}`,
+                    queryParams: { videoId: video._id } as any,
+                  };
+                })
+                .concat(
+                  topic.assignments.map((assignment) => {
+                    return {
+                      route: `/courseExplanation/${this.courseId}/video-player/${this.moduleId}/${topic._id}`,
+                      queryParams: {
+                        assignmentId: assignment._id,
+                      } as any,
+                    };
+                  })
+                );
             }) || [];
         }),
         switchMap(() => this.route.queryParams),
@@ -89,25 +114,105 @@ export class VideoPlayerComponent implements OnInit, AfterViewChecked {
             this.currentContentIndex = this.contentRoutingList.findIndex(
               (video) => video.queryParams['videoId'] == this.videoId
             );
+          } else {
+            this.video = undefined;
+          }
 
-            if (this.currentContentIndex === 0) {
-              this.previousVideoRoute = undefined;
-            } else if (this.currentContentIndex > 0) {
-              this.previousVideoRoute =
-                this.contentRoutingList[this.currentContentIndex - 1];
-            }
-            if (
-              this.currentContentIndex ===
-              this.contentRoutingList.length - 1
-            ) {
-              this.nextVideoRoute = undefined;
-            } else if (
-              this.currentContentIndex <
-              this.contentRoutingList.length - 1
-            ) {
-              this.nextVideoRoute =
-                this.contentRoutingList[this.currentContentIndex + 1];
-            }
+          this.assignmentId = queryParams['assignmentId'];
+          if (this.assignmentId) {
+            this.currentContentIndex = this.contentRoutingList.findIndex(
+              (assignment) =>
+                assignment.queryParams['assignmentId'] == this.assignmentId
+            );
+
+            this.coursesService
+              .getAssignmentById(this.assignmentId)
+              .pipe(
+                tap((assignment) => {
+                  this.assignement = assignment;
+                  // Initialize userAnswers as an object keyed by questionId
+                  this.userAnswers = {};
+                  assignment.data.questions.forEach((question) => {
+                    if (question.type === 'msq') {
+                      // For multiple select questions, initialize an array of booleans for each option
+                      this.userAnswers[question._id] = question.options.map(
+                        () => false
+                      );
+                    } else if (question.type === 'truefalse') {
+                      // For true/false (single select), initialize as empty string
+                      this.userAnswers[question._id] = '';
+                    }
+                  });
+                })
+              )
+              .subscribe();
+          } else {
+            this.assignement = undefined;
+          }
+
+          if (this.currentContentIndex === 0) {
+            this.previousVideoRoute = undefined;
+          } else if (this.currentContentIndex > 0) {
+            this.previousVideoRoute =
+              this.contentRoutingList[this.currentContentIndex - 1];
+          }
+          if (this.currentContentIndex === this.contentRoutingList.length - 1) {
+            this.nextVideoRoute = undefined;
+          } else if (
+            this.currentContentIndex <
+            this.contentRoutingList.length - 1
+          ) {
+            this.nextVideoRoute =
+              this.contentRoutingList[this.currentContentIndex + 1];
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  submitAssignment() {
+    console.log(this.userAnswers);
+    const answers = {
+      assignmentId: this.assignmentId,
+      answers: Object.keys(this.userAnswers).map((questionId) => {
+        const question = this.assignement?.data.questions.find(
+          (question) => question._id === questionId
+        );
+        const answer = this.userAnswers[questionId];
+
+        if (question?.type === 'msq') {
+          return {
+            questionId: question._id,
+            selectedOptions: answer
+              .map((option: any, index: number) => {
+                return option ? index : undefined;
+              })
+              .filter((option: any) => option !== undefined),
+          };
+        } else {
+          return {
+            questionId: question?._id,
+            selectedOptions: [answer],
+          };
+        }
+      }),
+    };
+    this.coursesService
+      .submitAssignment(answers)
+      .pipe(
+        catchError((error) => {
+          this.errorMessage = error.error.message;
+          return [];
+        }),
+        tap((res) => {
+          console.log(res);
+
+          if (!res.success) {
+            this.errorMessage = res.message;
+          } else {
+            this.topic!.assignments.find(
+              (assignment) => assignment._id === this.assignmentId
+            )!.isCompleted = true;
           }
         })
       )
